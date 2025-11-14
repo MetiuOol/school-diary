5dd9f1df57e82d5cd591cecaa0dc9e9c514529a5
MetiuOol <wasinski.mateusz@gmail.com>
2025-11-14 14:38:48 +0100

    delete sout from SchoolDiaryApplication.java

--- src/main/java/pl/kurs/schooldiary/SchoolDiaryApplication.java
+++ src/main/java/pl/kurs/schooldiary/SchoolDiaryApplication.java
@@ -9,7 +9,6 @@ public class SchoolDiaryApplication {
     public static void main(String[] args) {
         SpringApplication.run(SchoolDiaryApplication.class, args);
 
-        System.out.println("webhook test2");
     }
 
 
